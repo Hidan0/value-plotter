d75d8fe05c71f7f1b093ca8dad798c403d894f20
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2023-06-08 18:48:03 +0200

    [ADD] include y args

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use std::sync::{Arc, Mutex};
 use std::thread;
 
 use clap::{command, Parser};
+use eframe::egui::plot::{Line, Plot};
 use eframe::NativeOptions;
-use egui::plot::{Line, Plot};
 
 use self::measurements::Measurements;
 
@@ -14,20 +14,26 @@ mod measurements;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    /// Maximum seconds to display before the screen starts scrolling
-    #[arg(short, long)]
+    /// Maximum x to display before the screen starts scrolling
+    #[arg(short, long, default_value_t = 2000.)]
     window: f64,
+
+    /// y values to include in the plot
+    #[arg(short, long)]
+    include_y: Option<Vec<f64>>,
 }
 
 #[derive(Debug)]
 struct App {
     measurements: Arc<Mutex<Measurements>>,
+    include_y: Option<Vec<f64>>,
 }
 
 impl App {
-    fn new(window_size: f64) -> Self {
+    fn new(window_size: f64, include_y: Option<Vec<f64>>) -> Self {
         Self {
             measurements: Arc::new(Mutex::new(Measurements::new(window_size))),
+            include_y,
         }
     }
 }
@@ -35,7 +41,13 @@ impl App {
 impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            let plot = Plot::new("measurements");
+            let mut plot = Plot::new("measurements");
+
+            if let Some(ys) = &self.include_y {
+                for y in ys.iter() {
+                    plot = plot.include_y(*y);
+                }
+            }
 
             plot.show(ui, |plot_ui| {
                 plot_ui.line(Line::new(self.measurements.lock().unwrap().values()));
@@ -49,7 +61,7 @@ fn main() {
     let args = Args::parse();
     let native_options = NativeOptions::default();
 
-    let app = App::new(args.window);
+    let app = App::new(args.window, args.include_y);
     let ui_measurement = app.measurements.clone();
 
     thread::spawn(move || {
