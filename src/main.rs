ad8fd56d81b03fd51885fa859f837e5dd7377912
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2023-06-08 18:21:55 +0200

    [ADD] args parser

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,22 @@ use std::io::BufRead;
 use std::sync::{Arc, Mutex};
 use std::thread;
 
+use clap::{command, Parser};
 use eframe::NativeOptions;
 use egui::plot::{Line, Plot};
 
 use self::measurements::Measurements;
 
 mod measurements;
+
+/// A simple tool to monitor and plot values
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Maximum seconds to display before the screen starts scrolling
+    #[arg(short, long)]
+    window: f64,
+}
 
 #[derive(Debug)]
 struct App {
@@ -36,9 +46,10 @@ impl eframe::App for App {
 }
 
 fn main() {
+    let args = Args::parse();
     let native_options = NativeOptions::default();
 
-    let app = App::new(4000.);
+    let app = App::new(args.window);
     let ui_measurement = app.measurements.clone();
 
     thread::spawn(move || {
